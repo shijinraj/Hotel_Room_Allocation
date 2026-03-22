e0d5d607f787fb15b57aded3a55c35552a6493cd
shijin raj <shijinraj@gmail.com>
2026-03-22 19:14:32 +0100

    test: updated AllocationApplicationTests.java and added jacoco

--- src/test/java/com/shijin/hotel/room/allocation/AllocationApplicationTests.java
+++ src/test/java/com/shijin/hotel/room/allocation/AllocationApplicationTests.java
@@ -1,13 +1,24 @@
 package com.shijin.hotel.room.allocation;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
+
 @SpringBootTest
+@DisplayName( "AllocationApplication" )
 class AllocationApplicationTests {
 
 	@Test
+	@DisplayName( "Spring context loads successfully" )
 	void contextLoads() {
 	}
 
+	@Test
+	@DisplayName( "main method starts application without error" )
+	void mainMethodStartsApplication() {
+		assertThatCode( () -> AllocationApplication.main( new String[] {} ) )
+				.doesNotThrowAnyException();
+	}
 }
