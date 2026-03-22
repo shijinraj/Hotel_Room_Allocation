47fa2ad65a179a808294d41a1091b7b4a5d758b0
shijin raj <shijinraj@gmail.com>
2026-03-22 14:52:26 +0100

    featute: initial project setup

---
+++ src/test/java/com/shijin/hotel/room/allocation/AllocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shijin.hotel.room.allocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
